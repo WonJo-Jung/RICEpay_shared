662f831fd2b3ef1a31c6e9c4a0a3ffb5a56741f9
WonJo-Jung <unprecedentedgame222@gmail.com>
2025-09-24 17:13:49 +0900

    revert

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,3 @@ export * from './schemas';
 export * from './viem';
 export * from './erc20Abi';
 export * from './utils';
-export { normalizeEvmAddress } from './evm/address';
-export { AddressBookCreateSchema, AddressBookUpdateSchema, NetworkEnum } from './types/address-book';
-export type { CreateAddressBookDto, UpdateAddressBookDto, Network } from './types/address-book';
